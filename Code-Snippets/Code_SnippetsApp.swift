7ed07e4bd30eb303d7b55be827d203774ea59565
Karan Peshwani <karan.peshwani@meesho.com>
2025-07-31 17:27:24 +0530

    Initial Commit

---
+++ Code-Snippets/Code_SnippetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_SnippetsApp.swift
+//  Code-Snippets
+//
+//  Created by Karan Peshwani on 31/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Code_SnippetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
